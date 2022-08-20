3f6aca0e923aac1503ac94ddf65de07c5cc3cbce
Eduardo Mendes <eduargentin@outlook.com>
2022-08-20 03:34:46 -0300

    adicionando audio e imagens não corrompidas

--- imagens.js
+++ imagens.js
@@ -8,13 +8,13 @@ let hino;
 let pontos;
 
 function preload() {
-  imagemDaEstrada = loadImage("imagens/campo.png");
-  imagemMessi = loadImage("imagens/messi.png");
-  carro1 = loadImage("imagens/virgil.png");
-  carro2 = loadImage("imagens/marquinhos.png");
-  carro3 = loadImage("imagens/mbappe.png");
+  imagemDaEstrada = loadImage("./imagens/campo.png");
+  imagemMessi = loadImage("./imagens/messi.png");
+  carro1 = loadImage("./imagens/virgil.png");
+  carro2 = loadImage("./imagens/marquinhos.png");
+  carro3 = loadImage("./imagens/mbappe.png");
   carros = [carro1, carro2, carro3, carro1, carro2];
-  taça = loadImage("imagens/taça.png");
-  hino = loadSound("sons/hino.m4a");
-  pontos = loadSound("sons/pontos.wav");
+  taça = loadImage("./imagens/taça.png");
+  hino = loadSound("./sons/hino.m4a");
+  pontos = loadSound("./sons/pontos.wav");
 }
